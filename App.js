a80d4b10faf36da91dcbfe1e0265ccda0d1e668f
rzamora2 <rzamora@devtch.com>
2022-10-30 20:28:19 -0600

    Added project files

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      style={styles.container}
+      originWhitelist={['*']}
+      source={{uri: 'file:///android_asset/Arsha/index.html'}} //Funciona solo con android
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
